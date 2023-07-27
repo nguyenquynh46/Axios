ae010fffd1e02b13ceb959842c31e86c2b23b0cd
nguyenquynh46 <nguyenthiquynhhvnh93@gmail.com>
2023-07-27 14:03:11 +0700

    done layout 2 bang

--- AjaxC03/Home/home.js
+++
@@ -1,8 +0,0 @@
-function loadLogin(){
-    let str=`<form> 
-    <lable>Username</lable><input type="text">
-    <lable>Password</lable><input type="text">
-    <button >Login</button>  <button >Register</button>
-           </form>`;
-    document.getElementById('display').innerHTML=str
-}
