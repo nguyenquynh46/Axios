fcb74feee708a062b7cda7d267e9599cdf1f3961
nguyenquynh46 <nguyenthiquynhhvnh93@gmail.com>
2023-07-27 13:58:16 +0700

    layout 2 bang lan1

---
+++ AjaxC03/Home/home.js
@@ -0,0 +1,8 @@
+function loadLogin(){
+    let str=`<form> 
+    <lable>Username</lable><input type="text">
+    <lable>Password</lable><input type="text">
+    <button >Login</button>  <button >Register</button>
+           </form>`;
+    document.getElementById('display').innerHTML=str
+}
